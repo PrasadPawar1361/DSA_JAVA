7ab5340bc1f350a65879c24821e18da7277da582
PrasadPawar1361 <prasadpawarpp1361@gmail.com>
2026-01-15 19:05:21 +0530

    Add .gitignore and remove compiled class files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # DSA_JAVA
 in this repo i am start to solve dsa questions everyday and  try to solve 1/2 questions each day ....
+
+
+DSA_JAVA/
+├── Arrays/
+│   ├── TwoSum.java
+│   ├── MaxSubarray.java
+├── Strings/
+├── LinkedList/
+├── Stack/
+├── Queue/
+├── Tree/
+├── Graph/
+├── DP/
+└── README.md
---
+++ learn_array/Palindrome.java
@@ -0,0 +1,33 @@
+package learn_array;
+
+class Palindrome {
+
+    public static boolean isPalindrome(int z) {
+
+        // Negative numbers are not palindrome
+        if (z < 0) {
+            return false;
+        }
+
+        int n = z;
+        int revnum = 0;
+
+        while (n > 0) {
+            int d = n % 10;
+            revnum = revnum * 10 + d;
+            n = n / 10;
+        }
+
+        return revnum == z;
+    }
+
+    public static void main(String[] args) {
+        int num = 121;
+
+        if (isPalindrome(num)) {
+            System.out.println(num + " is Palindrome");
+        } else {
+            System.out.println(num + " is NOT Palindrome");
+        }
+    }
+}
