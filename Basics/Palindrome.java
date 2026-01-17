1c75865bc33c9a2ecee11a59bee2ae99a64f9c44
PrasadPawar1361 <prasadpawarpp1361@gmail.com>
2026-01-17 23:00:39 +0530

    Day 2: solved power of to  program in Basics

--- Basics/Palindrome.java
+++ Basics/Palindrome.java
@@ -1,4 +1,4 @@
-package learn_array;
+package Basics;
 
 class Palindrome {
 
---
+++ Basics/power_of_2.java
@@ -0,0 +1,32 @@
+package Basics;
+
+class power_of_2
+     {
+      public static boolean power(int n)
+      {
+        if(n<1){
+            return false;
+        }else if(n==1){
+            return  true;
+
+        }else 
+            while(n%2==0){
+                n=n/2;
+            }
+            
+    
+          return n==1;
+      }
+    
+
+    public static void main(String[] args) {
+        int n =9 ;
+
+        if (power(n)) {
+            System.out.println(n + "  is power of two :- ");
+        } else {
+            System.out.println(n + "  is not power of two :- ");
+        }
+    
+      }
+     }
